96d4ed388f38243301d5dfe214d268ebb3228a6e
Daria Yats <dariayatsyniuk@gmail.com>
2025-04-21 16:29:44 +0200

    whole project

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Дарья Яцынюк on 25.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
