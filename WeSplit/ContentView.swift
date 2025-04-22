8e0bc7b95e8c9ec9bbcabd0654713eb324f679bb
Daria Yats <dariayatsyniuk@gmail.com>
2025-04-22 12:52:01 +0200

    update

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,31 +12,32 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var tipPercentage = 20
     @FocusState private var amountIsFocused: Bool
-   
-    
+
+
     var totalAmount: Double {
         let tipSelection = Double(tipPercentage)
-        let amount = checkAmount + tipSelection
-        
+        let tipValue = checkAmount / 100 * tipSelection
+        let amount = checkAmount + tipValue
+
         return amount
     }
-    
+
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
         let tipSelection = Double(tipPercentage)
-        
+
         let tipValue = checkAmount / 100 * tipSelection
         let grandTotal = checkAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-        
+
         return amountPerPerson
     }
-    
+
     var body: some View {
-        NavigationView {
+        NavigationStack {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople) {
@@ -45,30 +46,29 @@ struct ContentView: View {
                         }
                     }
                 }
-                
+
                 Section {
                     Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
+                    .pickerStyle(.navigationLink)
                 } header: {
                     Text("How much tip do you want to leave?")
                 }
-                
-                
+
+
                 Section {
-                    Text(totalAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(totalAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                         .foregroundColor(tipPercentage == 0 ? .red : .black)
-                    } header: {
+                } header: {
                     Text("Total amount")
                 }
-                    
-                
-                
-                
+
+
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
                 } header: {
                     Text("Amount per person")
                 }
